c5769f96848adaf34b5ba686e13cb17a915a1050
MikeVolzhanin <mishavolzhanin.mishavolzhanin2@gmail.com>
2026-02-24 19:37:45 +0300

    feat: create structure of service

---
+++ services/applicants-service/src/main/java/ru/volzhanin/applicantsservice/ApplicantsServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.volzhanin.applicantsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicantsServiceApplication.class, args);
+    }
+
+}
