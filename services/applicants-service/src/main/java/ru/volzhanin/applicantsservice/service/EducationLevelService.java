e10fbd518512a45da473646df85607c55a78937f
MikeVolzhanin <mishavolzhanin.mishavolzhanin2@gmail.com>
2026-03-08 14:41:02 +0300

    feat: education level service

---
+++ services/applicants-service/src/main/java/ru/volzhanin/applicantsservice/service/EducationLevelService.java
@@ -0,0 +1,53 @@
+package ru.volzhanin.applicantsservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.volzhanin.applicantsservice.dto.EducationLevelDto;
+import ru.volzhanin.applicantsservice.entity.EducationLevel;
+import ru.volzhanin.applicantsservice.mapper.EducationLevelMapper;
+import ru.volzhanin.applicantsservice.repository.EducationLevelRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EducationLevelService {
+    private final EducationLevelRepository educationLevelRepository;
+    private final EducationLevelMapper educationLevelMapper;
+
+    public List<EducationLevelDto> getAllEducationLevel() {
+        return educationLevelMapper.toDto(educationLevelRepository.findAll());
+    }
+
+    public EducationLevelDto getEducationLevelById(Integer id) {
+        EducationLevel entity = educationLevelRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Education level not found")
+        );
+        return educationLevelMapper.toDto(entity);
+    }
+
+    @Transactional
+    public EducationLevelDto create(EducationLevelDto dto) {
+        EducationLevel entity = educationLevelMapper.toEntity(dto);
+        return educationLevelMapper.toDto(educationLevelRepository.save(entity));
+    }
+
+    @Transactional
+    public EducationLevelDto updateEducationLevelById(Integer id, EducationLevelDto dto) {
+        EducationLevel entity = educationLevelRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Education level not found: " + id));
+
+        educationLevelMapper.updateEntityFromDto(dto, entity);
+
+        return educationLevelMapper.toDto(educationLevelRepository.save(entity));
+    }
+
+    @Transactional
+    public void delete(Integer id) {
+        EducationLevel entity = educationLevelRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Education level not found: " + id));
+
+        educationLevelRepository.delete(entity);
+    }
+}
