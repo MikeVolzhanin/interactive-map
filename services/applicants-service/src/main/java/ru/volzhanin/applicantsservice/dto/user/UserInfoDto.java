1b8dfaf0b7e54f7b21266e9832ac32c61e43d5d2
MikeVolzhanin <mishavolzhanin.mishavolzhanin2@gmail.com>
2026-04-20 15:48:17 +0300

    fix: delete unuseful imports

--- services/applicants-service/src/main/java/ru/volzhanin/applicantsservice/dto/user/UserInfoDto.java
+++ services/applicants-service/src/main/java/ru/volzhanin/applicantsservice/dto/user/UserInfoDto.java
@@ -1,6 +1,5 @@
 package ru.volzhanin.applicantsservice.dto.user;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- services/applicants-service/src/main/java/ru/volzhanin/applicantsservice/mapper/UserMapper.java
+++ services/applicants-service/src/main/java/ru/volzhanin/applicantsservice/mapper/UserMapper.java
@@ -1,13 +1,7 @@
 package ru.volzhanin.applicantsservice.mapper;
 
 import org.springframework.stereotype.Component;
-import ru.volzhanin.applicantsservice.entity.Interest;
-import ru.volzhanin.applicantsservice.entity.User;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Component
 public class UserMapper {
-
 }
